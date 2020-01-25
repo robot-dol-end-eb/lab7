4d13d476d2d975f2c7dce795f511e1325e0720e5
robot-dol-and-eb <bikoluso@gmail.com>
2020-01-25 14:01:15 +0300

    laba4

--- laba1/Program.cs
+++ laba1/Program.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 
 namespace laba1
 {
+  
     class Program
     {
         static void Main(string[] args)
         {
-            
+            double a = 0, b = 0, c = 0;
+            Console.WriteLine("Введите 3 параметра a, b и с");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            MyLog.log("Введено уравнение: " + a + "X^2+" + b + "X+" + c + "=0");
+            try
+            {
+                QuaEquation calc = new QuaEquation();
+                List<double> res = calc.solve(a, b, c);
+                String s = "Корни уравнения: ";
+                foreach (double i in res)
+                {
+                    s += i + " ";
+                }
+                MyLog.log(s);
+            }
+            catch (VasikhinException e)
+            {
+                MyLog.log("Ошибка: " + e.Message);
+            }
+            MyLog.write();
             Console.ReadKey();
         }
     }
