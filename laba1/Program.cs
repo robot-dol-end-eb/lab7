1e48692b1c3e6a484d19b560edef2f3dfea7d729
robot-dol-and-eb <bikoluso@gmail.com>
2020-01-25 13:48:47 +0300

    laba1

---
+++ laba1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace laba1
+{
+   class A
+    {
+
+    }
+    class B
+    {
+        Object first, second;
+        public B(Object first, Object second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new A();
+            A a2 = new A();
+            A a3 = new A();
+            B b1 = new B(new A(), new A());
+            B b2 = new B(b1, new A());
+        }
+    }
+}
+
